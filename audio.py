fb05ce74271a356631d39b26d4c48fd3e4cd0455
mordren <joaokko@gmail.com>
2025-10-22 00:21:49 -0300

    começando testes do audio.py

--- audio.py
+++ audio.py
@@ -13,7 +13,7 @@ from pathlib import Path
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 try:
-    from audio import AudioManager
+    from audio_manager import AudioManager
 
 except ImportError as e:
     print(f"❌ Erro ao importar AudioManager: {e}")
